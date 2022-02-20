a0fca727301601d2c9f2aaec681d0b5c4df82b81
Klaudia <klaudia.laskowska99@gmail.com>
2022-02-20 01:57:15 +0100

    Fix navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,11 +3,14 @@
         <div class="container-fluid p-0 m-0 w-100">
             <div class="row w-100">
                 <div class="col-12 col-md-11 offset-md-1 pe-0">
-                    <div class="row mx-3 mx-sm-5 ms-md-0">
-                        <div class="col text-start d-flex align-items-center">
-                            <a class="navbar-brand" href="#">KLAUDIA LASKOWSKA</a>
+                    <div class="row mx-1 mx-sm-5 ms-md-0">
+                        <div class="col text-start ms-2">
+                            <div class="row">
+                                <a class="navbar-brand ps-2 " href="#">KLAUDIA LASKOWSKA</a>
+                            </div>
+
                         </div>
-                        <div class="col text-end p-0 my-auto">
+                        <div class="col text-end ms-auto p-0 my-auto">
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                 <i class="fa fa-bars"></i>
                             </button>
@@ -35,6 +38,25 @@
 
         </div>
     </nav>
+
+
+<!--    <nav class="navbar navbar-expand-lg navbar-light bg-light">-->
+<!--        <div class="container-fluid">-->
+<!--            <a class="navbar-brand" href="#">Navbar</a>-->
+<!--            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--                <span class="navbar-toggler-icon"></span>-->
+<!--            </button>-->
+<!--            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
+<!--                <div class="navbar-nav">-->
+<!--                    <a class="nav-link active" aria-current="page" href="#">Home</a>-->
+<!--                    <a class="nav-link" href="#">Features</a>-->
+<!--                    <a class="nav-link" href="#">Pricing</a>-->
+<!--                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </nav>-->
+
 </template>
 
 <script>
@@ -46,8 +68,7 @@ export default {
 <style scoped>
 
 .navbar {
-    height: 10vh;
-    min-height: 60px;
+    min-height: max(60px, 10vh);
 }
 
 .navbar-toggler {
@@ -56,5 +77,17 @@ export default {
 
 .navbar-toggler:focus {
     box-shadow: none;
+}
+
+.navbar-brand {
+    font-size: 18px;
+}
+
+.red {
+    background-color: red;
+}
+
+.blue {
+    background-color: blue;
 }
 </style>
