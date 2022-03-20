552b15ca5aab7f0f3b68e8659c665e55f5e68b36
Klaudia <klaudia.laskowska99@gmail.com>
2022-03-20 02:03:12 +0100

    Fix scrolling when going back/forward

--- src/router/index.js
+++ src/router/index.js
@@ -24,8 +24,12 @@ const routes = [
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
-    scrollBehavior() {
-        return { top: 0 }
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return { top: 0 }
+        }
     },
 })
 
