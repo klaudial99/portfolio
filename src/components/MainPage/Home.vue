0680207462a331a9e3afc29c8315514377db32b6
Klaudia <klaudia.laskowska99@gmail.com>
2022-04-09 17:11:33 +0200

    Fix home view

--- src/components/MainPage/Home.vue
+++ src/components/MainPage/Home.vue
@@ -1,66 +1,66 @@
 <template>
+    <div class="container h-100">
+        <div class="row mt-1 mt-lg-0 h-85" >
 
-    <div class="row h-100 w-100 mt-1 mt-lg-0" >
+            <div class="col-12 col-lg-7 d-flex justify-content-center align-items-center">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-11 col-lg-10">
+                        <div id="left-text" class="text-start mt-4">
+                            <span
+                                v-if="isLoaded"
+                                data-aos="zoom-in"
+                                data-aos-duration="1500"
+                                id="title"
+                                class="title-text"
+                            >
+                                Hi, I'm Klaudia.
+                            </span>
+                            <p
+                                v-if="isLoaded"
+                                data-aos="fade-up"
+                                data-aos-delay="800"
+                                data-aos-duration="700"
+                                data-aos-easing="ease"
+                                id="about"
+                                class="about-text"
+                            >
+                                {{ $t("home.about") }}
+                            </p>
 
-        <div class="col-12 col-lg-7 left d-flex justify-content-center align-items-center">
-            <div class="row d-flex justify-content-center w-100">
-                <div class="col-9">
-                    <div id="left-text" class="text-start mt-4">
-                        <span
-                            v-if="isLoaded"
-                            data-aos="zoom-in"
-                            data-aos-duration="1500"
-                            id="title"
-                            class="title-text"
-                        >
-                            Hi, I'm Klaudia.
-                        </span>
-                        <p
-                            v-if="isLoaded"
-                            data-aos="fade-up"
-                            data-aos-delay="800"
-                            data-aos-duration="700"
-                            data-aos-easing="ease"
-                            id="about"
-                            class="about-text"
-                        >
-                            {{ $t("home.about") }}
-                        </p>
-
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div class="col-12 col-lg-5 d-flex justify-content-center align-items-start align-items-md-center">
+                <img
+                    v-if="isLoaded"
+                    data-aos="fade-up-left"
+                    data-aos-delay="1500"
+                    data-aos-duration="700"
+                    data-aos-easing="ease"
+                    class="picture"
+                    src="@/assets/design.svg"
+                    alt="svg"
+                    width="450"
+                />
+            </div>
+
         </div>
-
-        <div class="col-12 col-lg-5 right d-flex justify-content-center align-items-start align-items-md-center">
-            <img
-                v-if="isLoaded"
-                data-aos="fade-up-left"
-                data-aos-delay="1500"
-                data-aos-duration="700"
-                data-aos-easing="ease"
-                class="picture"
-                src="@/assets/design.svg"
-                alt="svg"
-                width="450"
-            />
+        <div
+            data-aos="fade-up"
+            data-aos-anchor=".picture"
+            data-aos-delay="2000"
+            data-aos-duration="700"
+            data-aos-easing="ease"
+            class="row d-flex justify-content-center align-items-center container-fluid h-15 pb-3"
+            type="button"
+            @click="scrollDown"
+        >
+            <span>SCROLL DOWN</span>
+            <i id="scroll-down-icon" class="fa fa-chevron-down"></i>
         </div>
-
     </div>
-    <div
-        data-aos="fade-up"
-        data-aos-anchor=".picture"
-        data-aos-delay="2000"
-        data-aos-duration="700"
-        data-aos-easing="ease"
-        class="scroll-row row d-flex justify-content-center align-items-center container-fluid mt-auto"
-        type="button"
-        @click="scrollDown"
-    >
-        <span>SCROLL DOWN</span>
-        <i id="scroll-down-icon" class="fa fa-chevron-down"></i>
-    </div>
-
 </template>
 
 <script>
@@ -92,10 +92,6 @@ export default {
 
 <style scoped>
 
-.left {
-    min-height: 30vh;
-}
-
 #title {
     font-weight: bold;
     background: rgb(2,84,128);
@@ -115,27 +111,12 @@ export default {
 }
 
 @media (min-width: 1px) and (max-width: 576px) {
-    .left {
-        position: static;
-    }
-
-    #left-text {
-        position: static;
-        margin-top: 2.5rem;
-        text-align: justify;
-
-    }
-
     #title {
         line-height: 1.1;
     }
 
     #about {
         margin-top: 1.5rem;
-    }
-
-    .picture {
-        margin-left: 2rem;
     }
 }
 
@@ -151,10 +132,6 @@ export default {
     }
 }
 
-.scroll-row {
-    height: 10vh;
-}
-
 #scroll-down-icon {
     color: var(--VIOLET);
     animation: pulse-animation 2s infinite;
@@ -162,11 +139,19 @@ export default {
 
 @keyframes pulse-animation {
     0%, 100% {
-        transform: translateY(0);
+        transform: translateY(-5px);
     }
     50% {
-        transform: translateY(-10px);
+        transform: translateY(-15px);
     }
+}
+
+.h-85 {
+    height: 85%;
+}
+
+.h-15 {
+    height: 15%;
 }
 
 </style>
